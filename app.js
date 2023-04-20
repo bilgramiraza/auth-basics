5b02e825796aadc1ccf785b18a75d7a744238723
bilgramiraza <bilgramiraza@yahoo.com>
2023-04-20 16:39:15 +0530

    Added Log-in/out ability

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({secret:'cats', resave:false, saveUninitialized: true}));
 passport.use(
   new LocalStrategy(async(username,password,done)=>{
     try{
-      const user = User.findOne({username});
+      const user = await User.findOne({username});
 
       if(!user) return done(null, false, {message:'Incorrect Username'});
       
@@ -57,9 +57,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({extended:false}));
 
-app.get('/', (req,res)=> res.render('index'));
-app.get('/sign-up', (req,res)=>res.render('signUpForm')); 
+app.get('/', (req,res)=> res.render('index',{ user:req.user }));
 
+app.get('/sign-up', (req,res)=>res.render('signUpForm')); 
 app.post('/sign-up', async (req, res, next) =>{
   try{
     const user = new User({
@@ -72,5 +72,25 @@ app.post('/sign-up', async (req, res, next) =>{
     return next(err);
   }
 });
+
+app.post(
+  '/log-in',
+  passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/'
+  }),
+);
+
+app.get(
+  '/log-out',
+  (req, res, next)=>{
+    req.logout(function (err){
+      if(err) return next(err);
+      res.redirect('/');
+    });
+  }
+);
+
+
 
 app.listen(3000, ()=> console.log('app listening on Port 3000'));
